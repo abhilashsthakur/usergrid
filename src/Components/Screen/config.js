4dc42bf22e9ebe50d04d5db60b7c193b1becb144
abhilashsthakur <67498552+abhilashsthakur@users.noreply.github.com>
2021-06-18 19:59:55 +0530

    Update config.js

--- src/Components/Screen/config.js
+++ src/Components/Screen/config.js
@@ -8,9 +8,9 @@ export const SPECIFIC_WORK_API="https://insta-stories.herokuapp.com/work"
 export const defaultImage=`https://jamesriverdental.com/wp-content/uploads/2015/02/no-photo-male.jpg`
 
 // credientials for sending email
-export const YOUR_SERVICE_ID='service_v8n3bw8';
-export const YOUR_TEMPLATE_ID='template_dxbtbjm';
-export const YOUR_USER_ID='user_oU02VbXviNk517cGPpozl';
+export const YOUR_SERVICE_ID='YOUR_SERVICE_ID';
+export const YOUR_TEMPLATE_ID='YOUR_TEMPLATE_ID';
+export const YOUR_USER_ID='YOUR_USER_ID';
 
 export const PrimaryIcon='#f5222d'
 export const SecondaryIcon='#52c41a'
